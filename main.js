3f1dac76156a1d4c53f6dff5f287fc79717c9dbc
XT123456 <1804352454@qq.com>
2018-01-09 16:13:54 +0800

    导入：各种库

--- main.js
+++ main.js
@@ -1,60 +1,84 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path')
-const url = require('url')
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+	// Create the browser window.
+	/*
+	frame:false--隐藏外边框包括最大最小化等
+	*/
+	mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 600,
+		title: "虚拟化工程",
+		icon: path.join(__dirname,'/images/logo.png'),
+		autoHideMenuBar:true//自动隐藏菜单栏，按alt可切换展示
+	});
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+	mainWindow.setThumbarButtons([ //缩略图按钮功能，类似于网易云最小化的下一曲
+		{
+			tooltip: 'button1',
+			icon: path.join(__dirname, '/images/1.jpg'),
+			click() {
+				console.log("button1 clicked")
+			} //此处在主进程中打印（cmd中）
+		}, {
+			tooltip: 'button2',
+			icon: path.join(__dirname, '/images/2.jpg'),
+			click() {
+				console.log("button2 clicked", __dirname)
+			}
+		}
+	])
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('window-all-closed', function() {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+app.on('activate', function() {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
