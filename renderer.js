5b81a4479443bada3bda90e16e4a0c97a66a66e0
XT123456 <1804352454@qq.com>
2018-01-09 16:16:01 +0800

    修复

--- renderer.js
+++
@@ -1,9 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-console.log('1111111');
-$(function() {
-	$("#button").click(function() {
-		console.log("22222");
-	});
-});
